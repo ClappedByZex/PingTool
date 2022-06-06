17d084668d4bca459777d9c379a4d59809d3c741
ClappedByZex <106991492+ClappedByZex@users.noreply.github.com>
2022-06-06 10:03:08 -0700

    Update PingTool.bat

--- PingTool.bat
+++ PingTool.bat
@@ -1,7 +1,17 @@
 @echo off 
 color A
-title Ping Tool                         
+title Ping Tool
+
+echo             
+echo            
+echo  
+echo   
+echo  
+                                                
+
+                                 
 set /p IP= Enter IP/Domain:
+:top
 PING  -n 1 %IP% | FIND "TTL="
 ping -n 2 -l 10 127.0.0.1 >nul
 GoTo top
