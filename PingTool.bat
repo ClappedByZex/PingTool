07a59380b0d0e36e8fbff3b9ce4d8fa9b8f758a9
ClappedByZex <106991492+ClappedByZex@users.noreply.github.com>
2022-06-06 10:05:27 -0700

    Update PingTool.bat

--- PingTool.bat
+++ PingTool.bat
@@ -1,16 +1,47 @@
-@echo off 
-color A
+@echo off
+goto :start
+:start
 title Ping Tool
+color a
+cls
+Echo Made By Zex
+echo.
+echo.
+set /p input=Website:
+if %input%==%input% goto :2
+:2
+cls
+echo Pinging %input%
+ping localhost -n 2 >nul
+ping %input%
+echo.
+echo.
+echo Done!
+echo.
+echo.
+pause`
 
-echo             
-echo            
-echo  
-echo   
-echo  
-                                                
+:start
+title Ping-Website
+@echo off
+color a
+cls
+Echo You can ping any Website you want! (google.com, youtube.com usw)
+echo.
+echo.
+set /p input=Website:
+if %input%==%input% goto :2
 
-                                 
-set /p IP= Enter IP/Domain:
-PING  -n 1 %IP% | FIND "TTL="
-ping -n 2 -l 10 127.0.0.1 >nul
-GoTo top
+:2
+cls
+echo Pinging %input%...
+ping localhost -n 3 >nul
+ping %input%
+echo.
+echo.
+echo.
+echo Done!
+echo.
+echo.
+echo.
+pause
